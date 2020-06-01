c730bd39897090b8e9e8bcf5e1ab525de64c9182
Christoffer Carlsson <christoffercarlsson@Christoffers-MacBook-Pro.local>
2020-06-01 17:45:38 +0200

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Christoffer Carlsson on 2020-06-01.
+//  Copyright © 2020 Christoffer Carlsson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
